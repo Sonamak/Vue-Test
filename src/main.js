043c3e660a806611b0b89af27c7db583f518b75f
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-03-23 16:35:29 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router'
+import store from './store'
+import '@/assets/css/bootstrap.css'
+import '@/assets/css/style.css'
+createApp(App).use(store).use(router).mount('#app')
